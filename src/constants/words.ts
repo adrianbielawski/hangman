ccb3512ef03ac1c974c6bfad196dc364efb9ca71
Adrian Bielawski <adrian@bielaw.ski>
2021-11-28 16:04:40 +0000

    Words added to constants

---
+++ src/constants/words.ts
@@ -0,0 +1,57 @@
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    category: 'Programming language',
+    wodrs: [
+      {
+        id: 0,
+        word: 'javascript',
+      },
+      {
+        id: 1,
+        word: 'typescript',
+      },
+      {
+        id: 2,
+        word: 'java',
+      },
+      {
+        id: 3,
+        word: 'python',
+      },
+      {
+        id: 4,
+        word: 'c',
+      },
+      {
+        id: 5,
+        word: 'go',
+      },
+      {
+        id: 6,
+        word: 'php',
+      },
+    ], 
+  },
+  {
+    category: 'Programming paradigm',
+    wodrs: [
+      {
+        id: 0,
+        word: 'imperative',
+      },
+      {
+        id: 1,
+        word: 'logical',
+      },
+      {
+        id: 2,
+        word: 'finctional',
+      },
+      {
+        id: 3,
+        word: 'object oriented',
+      },
+    ], 
+  },
+]
